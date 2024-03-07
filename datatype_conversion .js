2d4d213dea89ca1c353e9e74f2256c13079eedce
mehakgoel29 <mhkgoel29@gmail.com>
2024-03-07 22:16:12 +0530

    conversion

---
+++ datatype_conversion .js
@@ -0,0 +1,26 @@
+let age = "33abc"
+console.log(typeof age );
+
+let value= Number(age)   // conversion to number 
+console.log(typeof value);
+console.log(value);
+
+//"33"=>33
+// "33abc"=>NaN
+// true=> 1 ; false=>0
+
+
+let booleanValue = 1 
+let bool = Boolean(booleanValue)
+console.log(bool)
+console.log(typeof bool )
+
+// 1=> true ; 0=> false 
+// ""=>  false 
+// "hitesh" => true
+
+
+let num= 33
+let str = String(num)
+console.log(str)
+console.log(typeof str)
