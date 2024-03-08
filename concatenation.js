80e7d498976bffc844649f5d0d13bf0394af6548
mehakgoel29 <mhkgoel29@gmail.com>
2024-03-08 18:31:36 +0530

    concatenation

---
+++ concatenation.js
@@ -0,0 +1,31 @@
+let a="Hello"
+let b=" world"
+let c= a+b;
+console.log(c);
+
+console.log(1+"8");
+console.log("1"+2);
+
+console.log(1+6+"3");
+
+console.log(+true);
+
+let d,e,f;
+d=e=f=4*4;
+console.log(e);
+
+let counter=239;
+counter++;
+console.log(counter);
+
+let choice=90;
+++choice;
+console.log(choice);
+
+let x = 8;
+const y = ++x;
+console.log(x,y);
+
+let m = 8;
+const n = m++;
+console.log(m,n);
